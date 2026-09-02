54558ecdd30aab5e44f439d8c6d542a50eba153c
tasneem <tasneemghonemy@gmail.com>
2026-09-02 20:43:56 +0300

    session15

--- session15/js/script.js
+++ session15/js/script.js
@@ -1,3 +1,4 @@
+///////////////////////task1//////////////////////////
 
 let handleclickEvent = (element)=>{
   let listItems = document.querySelectorAll('.container .custome-list .custome-list-item');
@@ -6,5 +7,33 @@ let handleclickEvent = (element)=>{
     listItems[i].classList.remove('text-primary');
   }
   element.classList.add('text-primary');
-
 }
+//////////////////////task2///////////////////////////
+let users = [];
+let loginForom = document.getElementById('loginForom');
+loginForom.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  // console.log("submited");
+  // console.log(e.target[0].value); 
+   let user = {
+    name : e.target[0].value,
+    email : e.target[1].value
+   };
+    // console.log(user);
+  users.push(user);
+  let div = document.createElement('div');  
+  let h4 = document.createElement('h4');
+  let h5 = document.createElement('h5');
+
+  h4.innerText = user.name;
+  h5.innerText = user.email;
+
+  div.appendChild(h4);
+  div.appendChild(h5);
+
+  div.classList.add('bg-warning','rounded-3','p-2');
+let parentDiv = document.querySelector('.parent-div');
+parentDiv.appendChild(div)
+  // (document.querySelector('parent-div')).appendChild(div);
+
+})
